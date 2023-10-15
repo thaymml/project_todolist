91e0f1e691bb3082589552498c8ba048ef7877a1
Thayssa <thayssamml@outlook.com>
2023-10-15 14:04:26 -0300

    my new project

---
+++ todolist/src/main/java/br/com/thayssalima/todolist/user/UserController.java
@@ -0,0 +1,46 @@
+package br.com.thayssalima.todolist.user;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+/**
+ * Modificador
+ * public
+ * private
+ * preotected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        
+        var passwordHashed =  BCrypt.withDefaults().hashToString( 12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+
+}
